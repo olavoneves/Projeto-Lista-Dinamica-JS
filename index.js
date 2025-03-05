6680bac31a9f5aa06dfeef1aa70f65fcda561dbb
Olavo Neves <152447074+olavoneves@users.noreply.github.com>
2025-03-05 19:34:24 -0300

    Projeto-Paginas-Dinamica

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-const inputItem = document.getElementById("input-item")
+const inputItem = document.getElementById("input-item");
 
-console.log(inputItem.value);
+const botaoAdicionar = document.getElementById("adicionar-item"); // a variavel Const significa que a variavel é constante
+
+let contador = 0; // a variavel Let significa que a variavel vai ficar trocando de valor, ou seja, ela não é constante
+
+const listaDeCompras = document.getElementById("lista-de-compras");
+
+botaoAdicionar.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    if (inputItem.value === "") {
+        alert("Por favor, insira um item!");
+        return
+    }
+
+    // document.createElement para criar elementos dentro da variavel
+    const itemDaLista = document.createElement("li");
+
+    const containerItemDaLista = document.createElement("div");
+    containerItemDaLista.classList.add("lista-item-container");
+
+    const inputCheckbox = document.createElement("input");
+    
+    // .type é o atributo da nossa variavel inputCheckbox 
+    inputCheckbox.type = "checkbox";
+
+    // .id tambem é uma das opções de atributos
+    inputCheckbox.id = "checkbox-" + contador++; 
+    // ++ serve para adicionar +1 toda vez que att //
+
+    const nomeItem = document.createElement("p");
+    nomeItem.innerText = inputItem.value;
+
+    containerItemDaLista.appendChild(inputCheckbox);
+    containerItemDaLista.appendChild(nomeItem);
+    
+    itemDaLista.appendChild(containerItemDaLista);
+    listaDeCompras.appendChild(itemDaLista);
+
+})
+
