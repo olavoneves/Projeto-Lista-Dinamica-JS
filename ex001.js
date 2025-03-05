9b1fe4f148b708fde8cd78029d82ffab9506c3a4
Olavo Neves <152447074+olavoneves@users.noreply.github.com>
2025-03-05 17:59:53 -0300

    Modificando texto com JS

---
+++ ex001.js
@@ -0,0 +1,12 @@
+const mensagem = "Integração realizada com sucesso!";
+
+console.log(mensagem)
+
+function modificarTexto () {
+    document.querySelector("h1").textContent = "Texto alterado"
+} 
+
+/*
+const h1 = document.querySelector("h1");
+
+h1.innerHTML = "Texto alterado"; */
